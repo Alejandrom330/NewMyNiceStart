e3819576432714103b65d07081e395d01664e564
Alejandrom330 <alejandro.mateo.pereira@gmail.com>
2021-01-29 11:30:18 +0100

    Añadido Alert dialog, carta extendible, refresh en el main, al mantener pulsada la imagen aparece un menu con 2 opciones, añadido un menu en el main, y algo mas

--- app/src/main/java/uem/dam/mynicestart/MainActivity.java
+++ app/src/main/java/uem/dam/mynicestart/MainActivity.java
@@ -1,17 +1,31 @@
 package uem.dam.mynicestart;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.alespero.expandablecardview.ExpandableCardView;
 import com.bumptech.glide.Glide;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView foto;
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +34,28 @@ public class MainActivity extends AppCompatActivity {
 
         foto = findViewById(R.id.fotoPerfil);
 
+
         Glide.with(this)
                 .load(R.drawable.cielo)
                 .centerCrop().circleCrop()
                 .into(foto);
+
+        ImageView mycontext = (ImageView) foto;
+        registerForContextMenu(mycontext);
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+        ExpandableCardView card = findViewById(R.id.profile);
+        card.setOnExpandedListener(new ExpandableCardView.OnExpandedListener() {
+            @Override
+            public void onExpandChanged(View v, boolean isExpanded) {
+                Toast.makeText(MainActivity.this, isExpanded ? "Expanded!" : "Colapsed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 
     public void cambiarventana2(View view) {
@@ -34,4 +66,124 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(i);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+                item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.android:
+                Toast toast = Toast.makeText(this,"going CONTEXT ITEM",
+                        Toast.LENGTH_LONG );
+                toast.show();
+                return true;
+            case R.id.action_settings:
+                Toast toast2 = Toast.makeText(this,"going CONTEXT SETTINGS",
+                        Toast.LENGTH_LONG );
+                toast2.show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.android) {
+            Toast toast = Toast.makeText(this,"Abriendo Alert Dialog",Toast.LENGTH_SHORT);
+            toast.show();
+
+            showAlertDialogButtonClicked(MainActivity.this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+
+            //    opción TOAST
+            //
+            //          Toast toast0 = Toast.makeText(MainContextActivity.this, "going swipeREFRESH", Toast.LENGTH_LONG);
+            //          toast0.show();
+
+
+            //   opción SNACKBAR
+
+//           SUSTITUIR POR CONSTRAINT EN SU CASO
+//            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.activity_main_context);
+            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.activity_main_context);
+
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+
+        }
+    };
+
+    public void showAlertDialogButtonClicked(MainActivity view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+/*
+        builder.setTitle("Alert Dialog Prueba");
+        builder.setMessage("Esto es una prueba de Alert Dialog");
+        builder.setIcon(R.drawable.mani);
+*/
+        builder.setView(getLayoutInflater().inflate(R.layout.alertdialog_view, null));
+
+        builder.setPositiveButton("Positivo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Negativo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
